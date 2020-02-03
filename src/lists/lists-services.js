b4089dd8dfe6fbbb75707531ea0b25a7165b7484
ksdumont <ksdumont@gmail.com>
2020-02-03 15:46:04 +0100

    updated list service

--- src/lists/lists-services.js
+++ src/lists/lists-services.js
@@ -1,11 +1,14 @@
 const ListsService = {
-    getAllLists(knex) {
-        return knex.select('*').from('lists')
-    },
-    updateLists(knex, id, newFields) {
-        return knex('lists')
-        .where({id})
-        .update(newFields)
-    }
-}
-module.exports = ListsService
+  getAllLists(knex) {
+    return knex
+      .select("*")
+      .from("lists")
+      .orderBy("id");
+  },
+  updateLists(knex, id, newFields) {
+    return knex("lists")
+      .where({ id })
+      .update(newFields);
+  }
+};
+module.exports = ListsService;
