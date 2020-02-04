868b2eea8c9fbb5a443b1f60d2e768cb56c052b5
ksdumont <ksdumont@gmail.com>
2020-02-04 09:16:06 +0100

    config

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/iPlan",
-    CLIENT_ORIGIN: 'http://localhost:3000'
+    //CLIENT_ORIGIN: 'http://localhost:3000'
 }
