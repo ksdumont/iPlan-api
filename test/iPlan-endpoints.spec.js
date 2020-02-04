65bd89d77133c147092aea2a9d8a92c06944d3df
ksdumont <ksdumont@gmail.com>
2020-02-04 15:40:00 +0100

    endpoints added for tests

--- test/iPlan-endpoints.spec.js
+++ test/iPlan-endpoints.spec.js
@@ -14,11 +14,11 @@ describe("iPlan Endpoints", () => {
       });
       app.set("db", db);
     });
-    after("disconnect from db", () => db.destroy());
+    //after("disconnect from db", () => db.destroy());
   
     before("cleanup", () => db("iPlan-test").truncate());
   
-    afterEach("cleanup", () => db("iPlan-test").truncate());
+    //afterEach("cleanup", () => db("iPlan-test").truncate());
 
     describe("GET /api/lists", () => {
         context(`Given no lists`, () => {
@@ -43,5 +43,72 @@ describe("iPlan Endpoints", () => {
           });
         });
     })
+
+    describe("GET /api/trips", () => {
+      context(`Given no trips`, () => {
+        it(`responds with 200 and an empty list`, () => {
+          return supertest(app)
+            .get("/api/trips")
+            .expect(200, []);
+        })
+      })
+      context(`Given there are trips in the database`, () => {
+        const testTrips = fixtures.makeTripsArray();
+  
+        beforeEach("insert trips", () => {
+          return db.into("iPlan-test").insert(testTrips);
+        });
+  
+        it("gets the trips", () => {
+          return supertest(app)
+            .get("/api/trips")
+            .expect(200, testTrips);
+        });
+      });
+  })
+  describe("GET /api/members", () => {
+    context(`Given no members`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get("/api/members")
+          .expect(200, []);
+      })
+    })
+    context(`Given there are members in the database`, () => {
+      const testMembers = fixtures.makeMembersArray();
+
+      beforeEach("insert members", () => {
+        return db.into("iPlan-test").insert(testMembers);
+      });
+
+      it("gets the members", () => {
+        return supertest(app)
+          .get("/api/members")
+          .expect(200, testMembers);
+      });
+    });
+})
+describe("GET /api/tasks", () => {
+  context(`Given no tasks`, () => {
+    it(`responds with 200 and an empty list`, () => {
+      return supertest(app)
+        .get("/api/tasks")
+        .expect(200, []);
+    })
+  })
+  context(`Given there are tasks in the database`, () => {
+    const testTasks = fixtures.makeTasksArray();
+
+    beforeEach("insert tasks", () => {
+      return db.into("iPlan-test").insert(testTasks);
+    });
+
+    it("gets the tasks", () => {
+      return supertest(app)
+        .get("/api/tasks")
+        .expect(200, testTasks);
+    });
+  });
+})
 })
   
